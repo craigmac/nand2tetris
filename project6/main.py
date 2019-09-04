1796fa334e345a8ca96d31ebefa33a45d6d63937
C.D. MacEachern <craigmaceachern@fastmail.com>
2019-09-04 13:00:38 -0400

    project 6 start with python package

---
+++ project6/main.py
@@ -0,0 +1,18 @@
+"""main.py
+
+Author: C.D. MacEachern <craigm@fastmail.com>
+
+Based on 'packer' python library created for nand2tetris project 6.
+"""
+
+from packer import parse
+
+
+def main():
+    """Main entry point.
+    """
+    parser = parse.Parser()
+
+
+if __name__ == '__main__':
+    main()
